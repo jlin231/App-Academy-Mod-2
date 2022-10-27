bcc4100f3c30244e0f5acbcdaf8ea36dc0a3e76c
jlin231 <jonlin981@gmail.com>
2022-10-27 11:21:01 -0700

    tests

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -11,27 +11,68 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
   }
 
   hash(key) {
-    // Your code here
+    let str = sha256(key);
+    return parseInt(str.slice(0, 8), 16);
   }
 
   hashMod(key) {
-    // Your code here
+    //returns an index
+    return this.hash(key) % this.data.length;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    //for a specific key, get an index to put hash at and put it into the array
+    let newKey = new KeyValuePair(key, value)
+    let index = this.hashMod(key);
+
+    //check if index is already used in the data array
+    if (this.data[index] !== null) {
+      throw Error("hash collision or same key/value pair already exists!");
+    }
+    //check if key-value pair already exists 
+
+    //add key value pair
+    this.data[index] = newKey;
+    this.count++;
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    let newKey = new KeyValuePair(key, value);
+    let index = this.hashMod(key);
+    this.count++;
+    if (this.data[index]) newKey.next = this.data[index];
+    this.data[index] = newKey;
   }
 
   insert(key, value) {
-    // Your code here
+    let newKey = new KeyValuePair(key, value);
+    let index = this.hashMod(key);
+    //check if index is already used in array
+    if (!this.data[index]) {
+      this.data[index] = newKey;
+      this.count++;
+    }
+    else {
+      //see if key has already been used at this index, if so then update 
+      let current = this.data[index];
+      while (current) {
+        if (current.key === key) {
+          //replace value at current key
+          current.value = value;
+          return;
+        }
+        current = current.next
+      }
+      newKey.next = this.data[index];
+      this.data[index] = newKey;
+      this.count++;
+    }
   }
 
 }
